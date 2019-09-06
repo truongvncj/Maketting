cb979fa6667968b40e87ec7bbf0f6113b52837c7
truongvncj <truongvncj@gmail.com>
2019-09-06 12:24:51 +0700

    cm

--- Maketting/View/Login.cs
+++ Maketting/View/Login.cs
@@ -111,7 +111,7 @@ namespace Maketting.View
 
                         Model.Username user = new Model.Username();
                         int Ver = Model.Username.getVersion();
-                        if (Ver == 78)
+                        if (Ver == 80)
                         {
 
                             this.Hide();
